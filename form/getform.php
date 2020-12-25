ff5fd942a1655dbbf6f2e57d82dbdec55f7e6b66
demon90s <demon90s@163.com>
2020-12-26 00:26:55 +0800

    commit

---
+++ form/getform.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<!-- 通过 url 发送表单数据，可见 -->
+<form action="test_getform.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text"m name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ form/postform.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<!-- 通过 http post 发送表单数据，不可见 -->
+<form action="test_postform.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text"m name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
